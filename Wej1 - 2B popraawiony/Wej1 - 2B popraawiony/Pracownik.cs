30e23a99586bb53c24efcac3ab083a9d1a41f297
Bartuzi <bartuzi97@gmail.com>
2019-10-29 14:44:56 +0100

    Zadanie 2

---
+++ Wej1 - 2B popraawiony/Wej1 - 2B popraawiony/Pracownik.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Pracownik
+{
+    string imie;
+    string nazwisko;
+    double pensja;
+
+	public Pracownik(string imie, string nazwisko, double pensja)
+	{
+
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.pensja = pensja;
+
+	}
+
+    public string roczneZarobki()
+    {
+        string c;
+        c = "Roczna pensja " + imie + " " + nazwisko + " wynosi " + pensja + "pln.";
+        return c;
+
+    }
+}
---
+++ Wej1 - 2B popraawiony/Wej1 - 2B popraawiony/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Wej1___2B_popraawiony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Pracownik P1 = new Pracownik("Michal", "Trupel", 2000);
+            Pracownik P2 = new Pracownik("Tomasz", "Ator", 2000.51);
+
+            Console.WriteLine(P1.roczneZarobki());
+            Console.WriteLine(P2.roczneZarobki());
+
+        }
+    }
+}
