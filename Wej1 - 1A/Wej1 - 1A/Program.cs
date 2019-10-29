03b701d9ba4de3dd926d02f1aee9008b9ef17d01
Bartuzi <bartuzi97@gmail.com>
2019-10-29 13:54:33 +0100

    Zadanie 1

---
+++ Wej1 - 1A/Wej1 - 1A/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Wej1___1A
+{
+    //Zadanie 1
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj temeperature w stopniach Celsjusza");
+            double tem;
+            tem = int.Parse(Console.ReadLine());
+            Console.WriteLine("Temeratura w C: {0}", tem);
+            double f;
+            f = tem * 1.8 + 32;
+            Console.WriteLine("Temperatura w F: {0}", f);
+
+        }
+    }
+}
